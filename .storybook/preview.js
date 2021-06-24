7797b67c37c70c755c597ea4f46cab7afe261756
Stanislas Ormières <stan@stormier.ninja>
2021-06-24 17:48:35 +0200

    :lipstick: Add CSS variables, fonts and reset

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,7 @@
 import '../src/assets/variables-dsfr.css'
 import '../src/assets/fonts-dsfr.css'
 import '../src/assets/reset-dsfr.css'
+import '../src/assets/utils-dsfr.css'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   plugins: [
     require('postcss-flexbugs-fixes'),
+    require('postcss-preset-env')({
+      stage: 1,
+    }),
     require('autoprefixer')({
       flexbox: 'no-2009',
     }),
