ca9bb9173d792c21bab5bd8fd2dd73b2de100e5f
Stanislas Ormières <stan@stormier.ninja>
2021-06-24 14:22:13 +0200

    :wrench: Setup PostCSS in Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,15 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        }
+      }
+    },
   ],
   "core": {
     "builder": "webpack5"
