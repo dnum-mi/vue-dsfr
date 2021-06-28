021c0e57fb06142b8592b8b349b30db0ed6fa3a5
Stanislas Ormières <stan@stormier.ninja>
2021-06-28 10:22:53 +0200

    :wrench: Fix babel config
    { loose: true } option should be the same for all three plugins

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,20 @@ module.exports = {
   ],
   plugins: [
     [
-      '@babel/plugin-proposal-private-property-in-object', {
+      '@babel/plugin-proposal-private-property-in-object',
+      {
+        loose: true,
+      },
+    ],
+    [
+      '@babel/plugin-proposal-private-methods',
+      {
+        loose: true,
+      },
+    ],
+    [
+      '@babel/plugin-proposal-class-properties',
+      {
         loose: true,
       },
     ],
