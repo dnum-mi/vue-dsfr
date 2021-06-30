65b7453f12092d829d65dbe9d64ba558d4f177a0
plegaud-PC-perso <pierre.egaud@laposte.net>
2021-06-30 18:10:11 +0200

    DsfrInput

---
+++ src/components/DsfrInput/DsfrInput.vue
@@ -0,0 +1,53 @@
+<template>
+  <label
+    :class="{
+      'fr-label': true,
+      'invisible': !labelVisible,
+    }"
+    :for="id"
+  >
+    {{ label }}
+  </label>
+  <input
+    :id="id"
+    type="search"
+    class="fr-input"
+    :placeholder="placeholder"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    @keydown.esc="$emit('update:modelValue', '')"
+  >
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+import { getRandomId } from '../../utils/random-utils.js'
+
+export default defineComponent({
+  name: 'DsfrInput',
+  props: {
+    id: {
+      type: String,
+      default: getRandomId('search', 'input'),
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+    labelVisible: Boolean,
+    modelValue: {
+      type: String,
+      default: '',
+    },
+    placeholder: {
+      type: String,
+      default: '',
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
