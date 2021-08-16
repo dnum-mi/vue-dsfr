0a2238e03e4a09bbe4ede2bdebf8f592a5800d20
Stanislas Ormières <stan@stormier.ninja>
2021-08-17 01:14:46 +0200

    fix: :lipstick: Corrige reset-css pour les titres h*

--- src/stories/titles.stories.js
+++ src/stories/titles.stories.js
@@ -1,27 +1,43 @@
-import { defineComponent } from 'vue'
-
-const TitlesWrapper = defineComponent({
-  template: '<div><slot/></div>',
-})
-
 export default {
-  component: TitlesWrapper,
   title: 'Basic/Titres',
   argTypes: {
     dark: { control: 'boolean' },
   },
 }
 
-export const Titles = (args) => ({
-  components: { TitlesWrapper },
+export const TitresHn = (args) => ({
+  data () {
+    return args
+  },
   template: `
-  <TitlesWrapper>
-    <h1>Titre h1</h1>
-    <h2>Titre h2</h2>
-    <h3>Titre h3</h3>
-    <h4>Titre h4</h4>
-    <h5>Titre h5</h5>
-    <h6>Titre h6</h6>
-  </TitlesWrapper>
+    <div :data-rf-theme="dark ? 'dark' : ''" style="background-color: var(--w); color: var(--g800); padding: 1rem;">
+      <h1>Titre h1</h1>
+      <h2>Titre h2</h2>
+      <h3>Titre h3</h3>
+      <h4>Titre h4</h4>
+      <h5>Titre h5</h5>
+      <h6>Titre h6</h6>
+    </div>
   `,
 })
+TitresHn.args = {
+  dark: false,
+}
+
+export const TitresAlternatifs = (args) => ({
+  data () {
+    return args
+  },
+  template: `
+    <div :data-rf-theme="dark ? 'dark' : ''" style="background-color: var(--w); color: var(--g800); padding: 1rem;">
+      <div class="fr-display-xl">Titre xl</div>
+      <div class="fr-display-lg">Titre lg</div>
+      <div class="fr-display-md">Titre md</div>
+      <div class="fr-display-sm">Titre sm</div>
+      <div class="fr-display-xs">Titre xs</div>
+    </div>
+  `,
+})
+TitresAlternatifs.args = {
+  dark: false,
+}
