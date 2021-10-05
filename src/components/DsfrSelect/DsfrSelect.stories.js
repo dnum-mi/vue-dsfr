711b4c7d6cad7c7028ae91fa1d902988f552689a
Stanislas Ormières <stan@stormier.ninja>
2021-10-05 20:24:31 +0200

    docs: :memo: Améliore la documentation

--- src/components/DsfrSelect/DsfrSelect.stories.js
+++ src/components/DsfrSelect/DsfrSelect.stories.js
@@ -7,27 +7,27 @@ export default {
     dark: { control: 'boolean' },
     required: {
       control: 'boolean',
-      description: 'Option permettant de rendre ce champ de formulaire obligatoire et d\'assigner au label un astérisque afin de rendre ce changement visible',
+      description: 'Option permettant de rendre ce champ de formulaire obligatoire et d’assigner au label un astérisque afin de rendre ce changement visible',
     },
     label: {
       control: 'text',
-      description: 'Label associé au select, donne le focus sur ce dernier au clic',
+      description: 'Label associé au `select`, donne le focus sur ce dernier au clic',
     },
     options: {
       control: 'array',
-      description: 'Liste des options proposées par le select à lui passer sous forme de tableau',
+      description: 'Liste des options proposées par le `select` à lui passer sous forme de tableau',
     },
     description: {
       control: 'text',
-      description: 'Description optionnelle du select',
+      description: 'Description optionnelle du `select`',
     },
     successMessage: {
       control: 'text',
-      description: 'Message à afficher en situation de succès, sa présence change la couleur de la police d\'écriture',
+      description: 'Message à afficher en situation de succès, sa présence change la couleur de la police d’écriture',
     },
     errorMessage: {
       control: 'text',
-      description: 'Message à afficher en cas d\'erreur, sa présence change la couleur de la police d\'écriture',
+      description: 'Message à afficher en cas d’erreur, sa présence change la couleur de la police d’écriture',
     },
     modelValue: {
       control: 'text',
@@ -35,7 +35,7 @@ export default {
     },
     disabled: {
       control: 'boolean',
-      description: 'Option empêchant toute interaction avec le select',
+      description: 'Option empêchant toute interaction avec le `select`',
     },
   },
 }
@@ -75,11 +75,11 @@ Select.args = {
     'Option 5',
     'Option 6',
   ],
-  label: 'Selection d\'options',
-  description: 'Je suis une description, je décris, c\'est ma raison d\'être',
+  label: 'Selection d’options',
+  description: 'Je suis une description, je décris, c’est ma raison d’être',
   successMessage: '',
   errorMessage: '',
   disabled: false,
-  modelValue: undefined,
+  modelValue: 'Option 2',
   required: false,
 }
