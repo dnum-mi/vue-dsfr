1abc4aa7fbe37bb2c882d6df4ebefac15b98d894
DaBadBunny <clement.debroize@gmail.com>
2021-10-12 17:57:26 +0200

    feat: :sparkles: Implémente  media en 2 composants DsfrPicture et DsfrVideo
    Permet d'intégrer un contenu de type image, video

---
+++ src/components/DsfrMedia/DsfrPicture.spec.js
@@ -0,0 +1,33 @@
+import { render } from '@testing-library/vue'
+
+import DsfrPicture from './DsfrPicture.vue'
+
+describe('DsfrPicture', () => {
+  it('should render a figure with div and img with alt and src', () => {
+    // Given
+    const size = 'small'
+    const src = 'https://placekitten.com/300/200'
+    const alt = 'Alternative text'
+    const legend = 'Photographie d’un chaton'
+
+    // When
+    const { container, getByText } = render(DsfrPicture, {
+      props: {
+        size,
+        src,
+        alt,
+        legend,
+      },
+    })
+    const figureEl = container.querySelector('.fr-content-media')
+    const figcaptionEl = getByText(legend)
+    const imgEl = container.querySelector('.fr-responsive-img')
+
+    // Then
+    expect(figureEl).toHaveAttribute('aria-label', legend)
+    expect(figureEl.tagName).toBe('FIGURE')
+    expect(imgEl).toHaveAttribute('src', src)
+    expect(imgEl).toHaveAttribute('alt', alt)
+    expect(figcaptionEl.tagName).toBe('FIGCAPTION')
+  })
+})
