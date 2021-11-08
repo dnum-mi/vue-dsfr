baf928769409700dc28a40012de4beffc155b87f
DaBadBunny <clement.debroize@gmail.com>
2021-11-08 17:14:17 +0100

    feat: :sparkles: Implémente le composant "Mise en exergue"

---
+++ src/components/DsfrHighlight/DsfrHighlight.stories.js
@@ -0,0 +1,50 @@
+import DsfrHighlight from './DsfrHighlight.vue'
+
+export default {
+  component: DsfrHighlight,
+  title: 'Éléments/Mise en Exergue - Highlight',
+  argTypes: {
+    dark: {
+      control: 'boolean',
+      description: 'Permet de voir le composant dans les deux **thèmes** : **clair** (`false`, défaut) et **sombre** (`true`).\n\n*N.B. : Ne fait pas partie du composant.*',
+    },
+    text: {
+      control: 'text',
+      description: 'Texte de la mise en avant',
+    },
+    small: {
+      control: 'boolean',
+      description: 'Permet d’afficher le texte en petit',
+    },
+    large: {
+      control: 'boolean',
+      description: 'Permet d’afficher le texte en plus grand',
+    },
+  },
+}
+
+export const MiseEnExergue = (args) => ({
+  components: {
+    DsfrHighlight,
+  },
+
+  data () {
+    return { ...args }
+  },
+
+  template: `
+    <div :data-rf-theme="dark ? 'dark' : ''" style="background-color: var(--w);">
+      <DsfrHighlight
+        :text="text"
+        :small="small"
+        :large="large"
+      />
+    </div>
+  `,
+})
+
+MiseEnExergue.args = {
+  small: false,
+  large: false,
+  text: 'Texte original de la mise en exergue',
+}
