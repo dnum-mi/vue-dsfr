cd12c6774d1e48c9faa9ea5df6b9c7080bde0cf7
Stanislas Ormières <stan@stormier.ninja>
2021-11-22 18:45:26 +0100

    test: :white_check_mark: Améliore les tests avec un vrai router

--- src/components/DsfrNavigation/DsfrNavigationMenuItem.spec.js
+++ src/components/DsfrNavigation/DsfrNavigationMenuItem.spec.js
@@ -18,4 +18,23 @@ describe('DsfrNavigationMenuItem', () => {
     expect(liEl).not.toHaveClass('fr-menu__item--active')
     expect(liEl).toHaveAttribute('id')
   })
+
+  it('should render an active navigation item', () => {
+    const content = 'Contenu d’un item de menu de navigation'
+
+    const { getByText } = render(DsfrNavigationMenuItem, {
+      props: {
+        active: true,
+      },
+      slots: {
+        default: content,
+      },
+    })
+
+    const liEl = getByText(content)
+
+    expect(liEl).toHaveClass('fr-menu__item')
+    expect(liEl).toHaveClass('fr-menu__item--active')
+    expect(liEl).toHaveAttribute('id')
+  })
 })
