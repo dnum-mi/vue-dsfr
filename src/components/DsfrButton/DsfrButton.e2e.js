22e5bc4afb8ac2004e7be2d4ad2da697af0e2724
Stanislas Ormières <stan@stormier.ninja>
2021-11-25 18:42:17 +0100

    refactor: :recycle: Améliore la lisibilité

--- src/components/DsfrButton/DsfrButton.e2e.js
+++ src/components/DsfrButton/DsfrButton.e2e.js
@@ -1,14 +1,23 @@
 import { mount } from '@cypress/vue'
-import DsfrButton from './DsfrButton'
+import DsfrButton from './DsfrButton.vue'
+import VIcon from '../../icons.js'
+import '../../main.css'
 
 describe('DsfrButton', () => {
   it('Simple Button', () => {
+    const buttonText = 'Test button'
     mount(DsfrButton, {
+      global: {
+        components: {
+          VIcon,
+        },
+      },
       slots: {
-        default: 'Test button',
+        default: () => buttonText,
       },
     })
-
-    cy.get('button').contains('Test button').click()
+      .get('button')
+      .contains(buttonText)
+      .click()
   })
 })
