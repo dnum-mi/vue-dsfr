63880700101f10e7777a2fbe7127ade869f46f66
Claire Nollet <clairenlet@gmail.com>
2022-11-08 14:49:32 +0100

    fix: :bug: Add title prop for DsfrFileDownloadList component
    fix: :bug: Add title prop for DsfrFileDownloadList component
    
    Revert "feat: :sparkles: add onClick props for DsfrTag"
    
    This reverts commit f300d972ff31aeea171769b1a4dd4636184583e1.

---
+++ src/components/DsfrFileDownload/DsfrFileDownloadList.spec.js
@@ -0,0 +1,42 @@
+import { render } from '@testing-library/vue'
+
+import DsfrFileDownloadList from './DsfrFileDownloadList.vue'
+
+describe('DsfrFileDownloadList', () => {
+  it('should render a download link block', async () => {
+    const title = 'Liste de liens de téléchargement'
+    const files = [
+      {
+        title: 'Télécharger document 1',
+        format: 'PNG',
+        size: '250 Mo',
+        href: '#',
+      },
+      {
+        title: 'Télécharger document 2',
+        format: 'GIF',
+        size: '107 Ko',
+        href: '#',
+      },
+      {
+        title: 'Télécharger document 3',
+        format: 'ZIP',
+        size: '1,5 Go',
+        href: '#',
+      },
+    ]
+
+    const { container, getByText } = render(DsfrFileDownloadList, {
+      props: {
+        title,
+        files,
+      },
+    })
+
+    const downloadTitle = getByText(title)
+    const links = container.querySelectorAll('li')
+
+    expect((downloadTitle).parentNode).toHaveClass('fr-downloads-group')
+    expect(links).toHaveLength(3)
+  })
+})
