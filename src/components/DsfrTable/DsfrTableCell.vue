f300d972ff31aeea171769b1a4dd4636184583e1
Claire Nollet <clairenlet@gmail.com>
2022-11-08 14:49:32 +0100

    feat: :sparkles: add onClick props for DsfrTag

--- src/components/DsfrTable/DsfrTableCell.vue
+++ src/components/DsfrTable/DsfrTableCell.vue
@@ -35,6 +35,7 @@ export default defineComponent({
       :is="isComponent"
       v-if="isComponent"
       v-bind="field"
+      :on-click="field.onClick() || false"
     >
       {{ field.text }}
     </component>
