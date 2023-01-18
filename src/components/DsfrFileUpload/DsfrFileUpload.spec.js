4676b9cc312f679894e222f6ed163794d3a00027
Ambroise Maupate <ambroise@rezo-zero.com>
2023-01-18 13:43:41 +0100

    fix: Changed DSFR error/valid messages classes in other components 2/2

--- src/components/DsfrFileUpload/DsfrFileUpload.spec.js
+++ src/components/DsfrFileUpload/DsfrFileUpload.spec.js
@@ -19,7 +19,7 @@ describe('DsfrFileUpload', () => {
 
     getByText(label)
     getByLabelText(label)
-    const errorEl = container.querySelector('.fr-message--error')
+    const errorEl = container.querySelector('.fr-error-text')
 
     expect(errorEl).toBeNull()
   })
@@ -43,7 +43,7 @@ describe('DsfrFileUpload', () => {
     getByText(label)
     getByText(hint)
 
-    const errorEl = container.querySelector('.fr-message--error')
+    const errorEl = container.querySelector('.fr-error-text')
 
     expect(errorEl).toBeNull()
   })
@@ -69,7 +69,7 @@ describe('DsfrFileUpload', () => {
     getByText(label)
     getByText(error)
 
-    const errorEl = container.querySelector('.fr-message--error')
+    const errorEl = container.querySelector('.fr-error-text')
 
     expect(errorEl).toBeDefined()
   })
