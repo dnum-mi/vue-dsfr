e970023c99156233a109333c5513b7d5603dd473
Gildéric DERUETTE <gilderic.deruette@kleegroup.com>
2023-02-22 13:49:12 +0100

    fix([DsfrSkipLink]): :bug: Les liens d'évitement ne peuvent pas fonctionner avec les liens vers les paragraphes html (#)
    Car Vue les utilise déjà pour le router. Il faut donc implémenter le saut de paragraphe "à la main"
    
    Fix: #481

--- src/components/DsfrSkipLinks/DsfrSkipLinks.stories.js
+++ src/components/DsfrSkipLinks/DsfrSkipLinks.stories.js
@@ -10,7 +10,7 @@ export default {
     },
     links: {
       control: 'object',
-      description: 'Permet de passer le label du lien et l’ancre ciblée sous forme de tableau d’objets. Chaque objet aura 2 propriétés : `id` et `text`.',
+      description: 'Permet de passer le label du lien et l’id de l’élément html ciblée sous forme de tableau d’objets. Chaque objet aura 2 propriétés : `id` et `text`.',
     },
   },
 }
@@ -27,31 +27,32 @@ export const LiensDEvitement = (args) => ({
   },
 
   template: `
-    <DsfrSkipLinks
-      :links="links"
-    />
+  <DsfrSkipLinks
+  :links="links"
+  />
+  <h2 id="what">Qu'est-ce que le Lorem Ipsum?</h2>
+    <p>Le <strong>Lorem Ipsum</strong> est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p>
+  </div>
+  <h2 id="why">Pourquoi l'utiliser?</h2>
+  <p>On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L'avantage du Lorem Ipsum sur un texte générique comme 'Du texte. Du texte. Du texte.' est qu'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour 'Lorem Ipsum' vous conduira vers de nombreux sites qui n'en sont encore qu'à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement (histoire d'y rajouter de petits clins d'oeil, voire des phrases embarassantes).</p>
+  </div>
   `,
 
   mounted () {
     document.body.parentElement.setAttribute('data-fr-theme', this.dark ? 'dark' : 'light')
   },
-
 })
 
 LiensDEvitement.args = {
   dark: false,
   links: [
     {
-      id: '#header',
-      text: 'Allons au header',
+      id: 'what',
+      text: 'Allons à la question du pourquoi ?',
     },
     {
-      id: '#content',
-      text: 'Allons au content',
-    },
-    {
-      id: '#footer',
-      text: 'Allons au footer',
+      id: 'why',
+      text: 'Allons à la question du comment ?',
     },
   ],
 }
