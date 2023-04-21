4a1149923dd272d1c618ed3c34cafc36c9a2d6dc
Ambroise Maupate <ambroise@rezo-zero.com>
2023-04-21 16:08:12 +0200

    fix(DsfrNavigationMegaMenu): Fixed DsfrNavigationMegaMenu to use real fr-collapsable behaviour.

--- src/components/DsfrNavigation/DsfrNavigationMegaMenu.vue
+++ src/components/DsfrNavigation/DsfrNavigationMegaMenu.vue
@@ -41,9 +41,63 @@ export default defineComponent({
 
   emits: ['toggle-id'],
 
+  data () {
+    return {
+      collapsing: false,
+      // Need to handle a separate data to add/remove the class after a RAF
+      cssExpanded: false,
+    }
+  },
+
   computed: {
     expanded () {
       return this.id === this.expandedId
+    },
+  },
+
+  watch: {
+    /*
+     * @see https://github.com/GouvernementFR/dsfr/blob/main/src/core/script/collapse/collapse.js
+     */
+    expanded (newValue, oldValue) {
+      if (newValue !== oldValue) {
+        if (newValue === true) {
+          // unbound
+          // @see https://github.com/GouvernementFR/dsfr/blob/main/src/core/script/collapse/collapse.js#L33
+          this.$refs.collapse.style.setProperty('--collapse-max-height', 'none')
+        }
+        // We need to wait for the next RAF to be sure the CSS variable will be set
+        // DSFR use RAF too https://github.com/GouvernementFR/dsfr/blob/main/src/core/script/api/modules/render/renderer.js#L22
+        window.requestAnimationFrame(() => {
+          this.collapsing = true
+          this.adjust()
+          // We need to wait for the next RAF to be sure the animation will be triggered
+          window.requestAnimationFrame(() => {
+            this.cssExpanded = newValue
+          })
+        })
+      }
+    },
+  },
+
+  methods: {
+    /*
+     * @see https://github.com/GouvernementFR/dsfr/blob/main/src/core/script/collapse/collapse.js#L61
+     */
+    adjust () {
+      this.$refs.collapse.style.setProperty('--collapser', 'none')
+      const height = this.$refs.collapse.offsetHeight
+      this.$refs.collapse.style.setProperty('--collapse', -height + 'px')
+      this.$refs.collapse.style.setProperty('--collapser', '')
+    },
+    /*
+     * @see https://github.com/GouvernementFR/dsfr/blob/main/src/core/script/collapse/collapse.js#L25
+     */
+    onTransitionEnd () {
+      this.collapsing = false
+      if (this.expanded === false) {
+        this.$refs.collapse.style.removeProperty('--collapse-max-height')
+      }
     },
   },
 })
@@ -63,7 +117,12 @@ export default defineComponent({
     data-testid="mega-menu-wrapper"
     class="fr-collapse fr-mega-menu"
     tabindex="-1"
-    :class="{ 'fr-collapse--expanded': expanded }"
+    ref="collapse"
+    @transitionend="onTransitionEnd"
+    :class="{
+      'fr-collapse--expanded': cssExpanded, // Need to use a separate data to add/remove the class after a RAF
+      'fr-collapsing': collapsing,
+    }"
   >
     <div class="fr-container  fr-container--fluid  fr-container-lg">
       <button
