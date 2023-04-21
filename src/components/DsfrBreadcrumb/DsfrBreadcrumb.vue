3dca1edf3b1e35acab973f0553941aa002f9896f
Ambroise Maupate <ambroise@rezo-zero.com>
2023-04-21 16:21:16 +0200

    fix(DsfrBreadcrumb): Fixed DsfrBreadcrumb to use real fr-collapsable behaviour.

--- src/components/DsfrBreadcrumb/DsfrBreadcrumb.vue
+++ src/components/DsfrBreadcrumb/DsfrBreadcrumb.vue
@@ -24,8 +24,57 @@ export default defineComponent({
 
   data () {
     return {
-      hideButton: false,
+      collapsing: false,
+      // Need to handle a separate data to add/remove the class after a RAF
+      cssExpanded: false,
+      expanded: false,
     }
+  },
+
+  watch: {
+    /*
+     * @see https://github.com/GouvernementFR/dsfr/blob/main/src/core/script/collapse/collapse.js
+     */
+    expanded (newValue, oldValue) {
+      if (newValue !== oldValue) {
+        if (newValue === true) {
+          // unbound
+          // @see https://github.com/GouvernementFR/dsfr/blob/main/src/core/script/collapse/collapse.js#L33
+          this.$refs.collapse.style.setProperty('--collapse-max-height', 'none')
+        }
+        // We need to wait for the next RAF to be sure the CSS variable will be set
+        // DSFR use RAF too https://github.com/GouvernementFR/dsfr/blob/main/src/core/script/api/modules/render/renderer.js#L22
+        window.requestAnimationFrame(() => {
+          this.collapsing = true
+          this.adjust()
+          // We need to wait for the next RAF to be sure the animation will be triggered
+          window.requestAnimationFrame(() => {
+            this.cssExpanded = newValue
+          })
+        })
+      }
+    },
+  },
+
+  methods: {
+    /*
+     * @see https://github.com/GouvernementFR/dsfr/blob/main/src/core/script/collapse/collapse.js#L61
+     */
+    adjust () {
+      this.$refs.collapse.style.setProperty('--collapser', 'none')
+      const height = this.$refs.collapse.offsetHeight
+      this.$refs.collapse.style.setProperty('--collapse', -height + 'px')
+      this.$refs.collapse.style.setProperty('--collapser', '')
+    },
+    /*
+     * @see https://github.com/GouvernementFR/dsfr/blob/main/src/core/script/collapse/collapse.js#L25
+     */
+    onTransitionEnd () {
+      this.collapsing = false
+      if (this.expanded === false) {
+        this.$refs.collapse.style.removeProperty('--collapse-max-height')
+      }
+    },
   },
 })
 </script>
@@ -37,18 +86,23 @@ export default defineComponent({
     aria-label="vous êtes ici :"
   >
     <button
-      v-show="!hideButton"
+      v-show="!expanded"
       class="fr-breadcrumb__button"
-      :aria-expanded="hideButton"
+      :aria-expanded="expanded"
       :aria-controls="breadcrumbId"
-      @click="hideButton = !hideButton"
+      @click="expanded = !expanded"
     >
       Voir le fil d’Ariane
     </button>
     <div
       :id="breadcrumbId"
+      ref="collapse"
       class="fr-collapse"
-      :class="{ 'fr-collapse--expanded': hideButton }"
+      :class="{
+        'fr-collapse--expanded': cssExpanded, // Need to use a separate data to add/remove the class after a RAF
+        'fr-collapsing': collapsing,
+      }"
+      @transitionend="onTransitionEnd"
     >
       <ol class="fr-breadcrumb__list">
         <li
