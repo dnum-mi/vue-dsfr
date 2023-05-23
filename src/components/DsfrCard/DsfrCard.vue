2a6d46fe9b7cce15dbab3b0eb3e977b0df4547a5
Ambroise Maupate <ambroise@rezo-zero.com>
2023-05-23 10:42:25 +0200

    fix(DsfrCard): Fixed `horizontal` feature (missing `fr-card__header`), added external link, buttons and links-group features

--- src/components/DsfrCard/DsfrCard.vue
+++ src/components/DsfrCard/DsfrCard.vue
@@ -1,8 +1,10 @@
 <script>
 import { defineComponent } from 'vue'
+import DsfrButtonGroup from '@/components/DsfrButton/DsfrButtonGroup.vue'
 
 export default defineComponent({
   name: 'DsfrCard',
+  components: { DsfrButtonGroup },
 
   props: {
     imgSrc: {
@@ -21,6 +23,10 @@ export default defineComponent({
       type: String,
       default: 'Simple description',
     },
+    size: {
+      type: String,
+      default: 'md',
+    },
     detail: {
       type: String,
       default: 'details',
@@ -33,13 +39,30 @@ export default defineComponent({
       type: String,
       default: 'h3',
     },
+    buttons: {
+      type: Array,
+      default: () => [],
+    },
+    linksGroup: {
+      type: Array,
+      default: () => [],
+    },
     noArrow: Boolean,
     horizontal: Boolean,
   },
 
-  methods: {
-    goToTargetLink () {
-      this.$refs.title.querySelector('.fr-card__link').click()
+  computed: {
+    sm () {
+      return ['sm', 'small'].includes(this.size)
+    },
+    md () {
+      return ['md', 'medium'].includes(this.size)
+    },
+    lg () {
+      return ['lg', 'large'].includes(this.size)
+    },
+    externalLink () {
+      return typeof this.link === 'string' && this.link.startsWith('http')
     },
   },
 })
@@ -51,42 +74,95 @@ export default defineComponent({
     :class="{
       'fr-card--horizontal': horizontal,
       'fr-enlarge-link': !noArrow,
+      'fr-card--sm': sm,
+      'fr-card--lg': lg,
     }"
     data-testid="fr-card"
-    @click="goToTargetLink"
   >
     <div class="fr-card__body">
-      <component
-        :is="titleTag"
-        ref="title"
-        class="fr-card__title"
-      >
-        <RouterLink
-          :to="link"
-          class="fr-card__link"
-          data-testid="card-link"
-          @click="$event.stopPropagation()"
+      <div class="fr-card__content">
+        <component
+          :is="titleTag"
+          ref="title"
+          class="fr-card__title"
         >
-          {{ title }}
-        </RouterLink>
-      </component>
-      <p class="fr-card__desc">
-        {{ description }}
-      </p>
-      <p class="fr-card__detail">
-        {{ detail }}
-      </p>
-    </div>
-    <div class="fr-card__img">
-      <img
-        :src="imgSrc"
-        class="fr-responsive-img"
-        :alt="altImg"
-        data-testid="card-img"
+          <a
+            v-if="externalLink"
+            :href="link"
+            data-testid="card-link"
+            class="fr-card__link"
+          >{{ title }}</a>
+          <RouterLink
+            v-else-if="link"
+            :to="link"
+            class="fr-card__link"
+            data-testid="card-link"
+            @click="$event.stopPropagation()"
+          >
+            {{ title }}
+          </RouterLink>
+          <template v-else>
+            {{ title }}
+          </template>
+        </component>
+        <p class="fr-card__desc">
+          {{ description }}
+        </p>
+        <p class="fr-card__detail">
+          {{ detail }}
+        </p>
+      </div>
+
+      <div
+        v-if="buttons.length || linksGroup.length"
+        class="fr-card__footer"
       >
-      <!-- L'alternative de l'image (attribut alt) doit à priori rester vide car l'image est illustrative
-        et ne doit pas être restituée aux technologies d’assistance. Vous pouvez toutefois remplir l'alternative si vous
-        estimez qu'elle apporte une information essentielle à la compréhension du contenu non présente dans le texte -->
+        <dsfr-button-group
+          v-if="buttons.length"
+          :buttons="buttons"
+          inline-layout-when="lg"
+          :size="size"
+          reverse
+        />
+        <ul
+          v-if="linksGroup.length"
+          class="fr-links-group"
+        >
+          <li
+            v-for="singleLink in linksGroup"
+            :key="singleLink.link || singleLink.href || singleLink.to"
+          >
+            <a
+              :href="singleLink.link || singleLink.href || singleLink.to"
+              :class="{
+                'fr-link': true,
+                'fr-icon-arrow-right-line': true,
+                'fr-link--icon-right': true,
+                'fr-link--sm': sm,
+                'fr-link--lg': lg,
+              }"
+            >
+              {{ singleLink.label }}
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div
+      v-if="imgSrc"
+      class="fr-card__header"
+    >
+      <div class="fr-card__img">
+        <img
+          :src="imgSrc"
+          class="fr-responsive-img"
+          :alt="altImg"
+          data-testid="card-img"
+        >
+        <!-- L'alternative de l'image (attribut alt) doit à priori rester vide car l'image est illustrative
+          et ne doit pas être restituée aux technologies d’assistance. Vous pouvez toutefois remplir l'alternative si vous
+          estimez qu'elle apporte une information essentielle à la compréhension du contenu non présente dans le texte -->
+      </div>
     </div>
   </div>
 </template>
