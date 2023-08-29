3b4db3fad037f7997c84a76658d71c4f40313fe4
DaBadBunny <clement.debroize@gmail.com>
2023-08-29 15:24:41 +0200

    feat: :sparkles: Mise à jour du composant FileDownload

--- src/components/DsfrFileDownload/DsfrFileDownload.vue
+++ src/components/DsfrFileDownload/DsfrFileDownload.vue
@@ -5,8 +5,6 @@ export type DsfrFileDownloadProps = {
   size?: string
   href?: string
   download?: string
-  description?: string
-  block?: boolean
 }
 
 withDefaults(defineProps<DsfrFileDownloadProps>(), {
@@ -15,31 +13,17 @@ withDefaults(defineProps<DsfrFileDownloadProps>(), {
   size: '250 Ko',
   href: '#',
   download: '',
-  description: '',
 })
 </script>
 
 <template>
-  <div
-    class="fr-download"
-    :class="{ 'fr-enlarge-link fr-download--card': block }"
-  >
-    <p>
-      <a
-        :href="href"
-        :download="download"
-        class="fr-download__link"
-      > {{ title }}
-        <span class="fr-download__detail">
-          {{ format }} – {{ size }}
-        </span>
-      </a>
-    </p>
-    <p
-      v-if="block"
-      class="fr-download__desc"
-    >
-      {{ description }}
-    </p>
-  </div>
+  <a
+    :href="href"
+    :download="download"
+    class="fr-link  fr-link--download"
+  > {{ title }}
+    <span class="fr-link__detail">
+      {{ format }} – {{ size }}
+    </span>
+  </a>
 </template>
