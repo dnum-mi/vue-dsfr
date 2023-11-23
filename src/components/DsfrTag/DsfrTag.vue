63c253e22ad2f5764a91e21e893349be611d1a13
aureliebracco <abracco@ippon.fr>
2023-11-23 17:07:25 +0100

    fix(DsfrTag): correction mode sélectionnable

--- src/components/DsfrTag/DsfrTag.vue
+++ src/components/DsfrTag/DsfrTag.vue
@@ -11,6 +11,7 @@ export type DsfrTagProps = {
   link? : string
   tagName?: string
   icon? : string
+  selected? : boolean
   disabled? : boolean
   small? : boolean
   iconOnly? : boolean
@@ -44,6 +45,7 @@ const linkProps = computed(() => {
     :class="{
       'fr-tag--sm': small,
     }"
+    :aria-pressed="selected"
     v-bind="linkProps"
   >
     <VIcon
