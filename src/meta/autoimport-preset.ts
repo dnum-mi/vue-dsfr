2b9880721c107bdfa9941013b5867f32d9435b8c
Stanislas Ormières <stan@stormier.ninja>
2023-12-10 14:13:32 +0100

    feat: :sparkles: ajoute un preset autoimport et un resolver
    - l’autoimport est utilisable dans Vite et dans nuxt (pour les composables)
    - le component resolver est utilisable dans Vite

---
+++ src/meta/autoimport-preset.ts
@@ -0,0 +1,8 @@
+// Autoimport preset for unplugin-auto-import plugin for Vite or Nuxt for VueDsfr composables
+export const vueDsfrAutoimportPreset = {
+  from: '@gouvminint/vue-dsfr',
+  imports: [
+    'useScheme',
+    'useTabs',
+  ],
+} as const
