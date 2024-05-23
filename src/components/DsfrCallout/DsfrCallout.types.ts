caf6843abd978b6eea8b7093a8c83f7f6f7f8705
vantunes <vantunes@ippon.fr>
2024-05-23 09:42:30 +0200

    fix: callout title is optional

--- src/components/DsfrCallout/DsfrCallout.types.ts
+++ src/components/DsfrCallout/DsfrCallout.types.ts
@@ -2,7 +2,7 @@ import type { OhVueIcon as VIcon } from 'oh-vue-icons'
 import type { DsfrButtonProps } from '../DsfrButton/DsfrButton.types'
 
 export type DsfrCalloutProps = {
-  title: string
+  title?: string
   content: string
   titleTag?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
   button?: DsfrButtonProps
