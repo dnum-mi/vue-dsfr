074ca42a29b7ed2ceffd2f7ccc17d46cd0e71d39
iNeoO <valere.pique@sg.social.gouv.fr>
2024-07-05 10:47:40 +0200

    feat: ✨ Mettre à jour DsfrNotice avec l'udpate du Dsfr en 1.12

--- src/components/DsfrNotice/DsfrNotice.types.ts
+++ src/components/DsfrNotice/DsfrNotice.types.ts
@@ -1,4 +1,6 @@
 export type DsfrNoticeProps = {
   title?: string
+  desc?: string
   closeable?: boolean
+  type?: 'info' | 'warning' | 'alert' | 'weather-orange' | 'weather-red' | 'weather-purple' | 'witness' | 'kidnapping' | 'attack' | 'cyberattack'
 }
