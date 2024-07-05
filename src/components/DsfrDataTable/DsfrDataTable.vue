5844e5bf44b6a566720e13192c086cdbeb4a444d
clement.debroize <clement.debroize@interieur.gouv.fr>
2024-07-05 17:07:47 +0200

    feat: :sparkles: Mise en place composant DataTable v.1.12.0

---
+++ src/components/DsfrDataTable/DsfrDataTable.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import type { DsfrDataTableProps } from './DsfrDataTable.types'
+
+const props = withDefaults(defineProps<DsfrDataTableProps>(), {
+  headersRow: () => [],
+  contentRows: () => [],
+  headersColumn: () => [],
+  selectableRows: () => [],
+  topActionsRow: () => [],
+  bottomActionsRow: () => [],
+  currentPage: 1,
+  resultsPerPage: 10,
+})
+</script>
+
+<template>
+</template>
+
+<style scoped>
+
+</style>
