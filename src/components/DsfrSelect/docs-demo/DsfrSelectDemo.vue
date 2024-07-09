aa098ccd7defb34d94f9c415109ac52f33c566d3
iNeoO <valere.pique@sg.social.gouv.fr>
2024-07-09 11:51:47 +0200

    docs: 📝 Add DsfrSelect.md

---
+++ src/components/DsfrSelect/docs-demo/DsfrSelectDemo.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import DsfrSelect from '../DsfrSelect.vue'
+</script>
+
+<template>
+  <div
+    class="flex flex-col"
+  >
+    <DsfrSelect
+      label="Label du select"
+      :options="[1, 2, 3, 4, 5]"
+    />
+    <DsfrSelect
+      label="Label du select"
+      description="Description du select"
+      :options="['Value 1', 'Value 2', 'Value 3', 'Value 4', 'Value 5']"
+    />
+    <DsfrSelect
+      label="Label du select"
+      border-bottom
+      required
+      :options="[
+        { value: 'Value 1', text: 'Text 1' },
+        { value: 'Value 2', text: 'Text 2' },
+        { value: 'Value 3', text: 'Text 3' },
+        { value: 'Value 4', text: 'Text 4' },
+        { value: 'Value 5', text: 'Text 5' },
+      ]"
+    />
+  </div>
+</template>
