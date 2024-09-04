f93cdd6d778755af06de86fb732327e0f03dc571
Stanislas Ormières <stan@stormier.ninja>
2024-09-04 18:43:17 +0200

    docs: 🐛 corrige bug du build de l’app demo

--- demo-app/views/AboutUs.vue
+++ demo-app/views/AboutUs.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 
-import MyIcon from '../../src/components/VIcon.vue'
+import MyIcon from '../../src/components/VIcon/VIcon.vue'
 
 const title = ref('SVG À propos')
 setTimeout(() => {
