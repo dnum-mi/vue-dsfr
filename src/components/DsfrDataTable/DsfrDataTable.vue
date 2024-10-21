4a3285e36ee326015a2aac8e0e032c244dc86cc8
Stanislas Ormières <stan@stormier.ninja>
2024-10-21 15:16:15 +0200

    chore: 🚨 fix lint issue

--- src/components/DsfrDataTable/DsfrDataTable.vue
+++ src/components/DsfrDataTable/DsfrDataTable.vue
@@ -64,6 +64,8 @@ const pages = computed<Page[]>(() => props.pages ?? Array.from({ length: pageCou
 const lowestLimit = computed(() => currentPage.value * rowsPerPage.value)
 const highestLimit = computed(() => (currentPage.value + 1) * rowsPerPage.value)
 
+const sortedBy = defineModel<string | undefined>('sortedBy', { default: undefined })
+const sortedDesc = defineModel('sortedDesc', { default: false })
 function defaultSortFn (a: string | DsfrDataTableRow, b: string | DsfrDataTableRow) {
   const key = sortedBy.value ?? props.sorted
   // @ts-expect-error TS7015
@@ -76,9 +78,6 @@ function defaultSortFn (a: string | DsfrDataTableRow, b: string | DsfrDataTableR
   }
   return 0
 }
-
-const sortedBy = defineModel<string | undefined>('sortedBy', { default: undefined })
-const sortedDesc = defineModel('sortedDesc', { default: false })
 function sortBy (key: string) {
   if (!props.sortableRows || (Array.isArray(props.sortableRows) && !props.sortableRows.includes(key))) {
     return
