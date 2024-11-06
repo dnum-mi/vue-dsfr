e837b2592baa45e99a662578c48087ffaead9c98
Yohann Valentin <pro@yohannvalentin.com>
2024-11-06 14:57:22 +0100

    fix: :bug: ajoute de l'attribut `id` au composant `DsfrNavigationItem` depuis le composant `DsfrNavigation`

--- src/components/DsfrNavigation/DsfrNavigationItem.spec.ts
+++ src/components/DsfrNavigation/DsfrNavigationItem.spec.ts
@@ -1,13 +1,17 @@
 import { render } from '@testing-library/vue'
 // import '@gouvfr/dsfr/dist/core/core.module.js'
+import { useId } from 'vue-demi'
 
 import DsfrNavigationItem from './DsfrNavigationItem.vue'
 
 describe('DsfrNavigationItem', () => {
   it('should render a navigation item', () => {
     const content = 'Contenu d’un item de menu de navigation'
-
+    const id = useId()
     const { getByText } = render(DsfrNavigationItem, {
+      props: {
+        id,
+      },
       slots: {
         default: content,
       },
@@ -18,5 +22,6 @@ describe('DsfrNavigationItem', () => {
     expect(liEl).toHaveClass('fr-nav__item')
     expect(liEl).not.toHaveClass('fr-nav__item--active')
     expect(liEl).toHaveAttribute('id')
+    expect(liEl.id).toBe(id)
   })
 })
