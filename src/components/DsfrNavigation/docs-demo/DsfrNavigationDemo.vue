0327ab7669782d4f36feec00af7477b80461a083
DaBadBunny <clement.debroize@gmail.com>
2025-07-21 15:23:14 +0200

    docs: :memo: Ajout de la documentation vitepress pour DsfrNavigation

---
+++ src/components/DsfrNavigation/docs-demo/DsfrNavigationDemo.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import DsfrNavigation from '../DsfrNavigation.vue'
+
+const navItemsDemo = [
+  {
+    to: '/accueil',
+    text: 'Accueil',
+  },
+  {
+    to: '/tableau-de-bord',
+    text: 'Tableau de bord',
+  },
+  {
+    to: '/historique',
+    text: 'Historique',
+  },
+]
+</script>
+
+<template>
+  <DsfrNavigation
+    label="Menu principal démo"
+    :nav-items="navItemsDemo"
+  />
+</template>
