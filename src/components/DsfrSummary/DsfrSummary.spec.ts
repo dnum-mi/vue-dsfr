6899b3ebfc5f63045158c5d6ac0118180286963c
Joel Pagniez <joel.pagniez@gmail.com>
2026-05-20 17:45:55 +0200

    test(DsfrSummary): ✅ ajoute les tests pour la prop titleTag

--- src/components/DsfrSummary/DsfrSummary.spec.ts
+++ src/components/DsfrSummary/DsfrSummary.spec.ts
@@ -21,4 +21,32 @@ describe('DsfrSummary', () => {
     expect(anchorTest).toHaveProperty('href')
     expect(anchorTest.href).toMatch('#second')
   })
+
+  it('should render the title with the default tag h2', () => {
+    // Given
+    const title = 'Sommaire'
+    const anchors = [{ link: '#first', name: 'Première étape' }]
+
+    // When
+    const { getByText } = render(DsfrSummary, {
+      props: { title, anchors },
+    })
+
+    // Then
+    expect(getByText(title).tagName.toLowerCase()).toBe('h2')
+  })
+
+  it.each(['h2', 'h3', 'h4', 'h5', 'p'] as const)('should render the title as <%s>', (titleTag) => {
+    // Given
+    const title = 'Sommaire'
+    const anchors = [{ link: '#first', name: 'Première étape' }]
+
+    // When
+    const { getByText } = render(DsfrSummary, {
+      props: { title, anchors, titleTag },
+    })
+
+    // Then
+    expect(getByText(title).tagName.toLowerCase()).toBe(titleTag)
+  })
 })
